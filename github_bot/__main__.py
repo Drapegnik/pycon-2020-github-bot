759612f16ba4a2428018c37240e700adb0ccdabb
Ivan Pazhitnykh <ivan.pazhitnykh@gmail.com>
2020-02-21 15:50:27 +0300

    add issue autorespone

--- github_bot/__main__.py
+++ github_bot/__main__.py
@@ -1,11 +1,26 @@
 from octomachinery.app.server.runner import run as run_app
+from octomachinery.routing import process_event_actions
+from octomachinery.routing.decorators import process_webhook_payload
+from octomachinery.runtime.context import RUNTIME_CONTEXT
 
-# https://github-bots.herokuapp.com
 
-if __name__ == '__main__':
+@process_event_actions("issues", {"opened"})
+@process_webhook_payload
+async def on_issue_opened(*, issue, **_kw):
+    """Whenever an issue is opened, greet the author and say thanks."""
+    github_api = RUNTIME_CONTEXT.app_installation_client
+    url = issue["comments_url"]
+    author = issue["user"]["login"]
+    message = (
+        f"Thanks for the report @{author}! " "I will look into it ASAP! (I'm a bot 🤖)."
+    )
+    await github_api.post(url, data={"body": message})
+
+
+if __name__ == "__main__":
+    # https://github-bots.herokuapp.com
     run_app(
-        name='pycon-2020-github-bot',
-        version='1.0.0',
-        # url='https://github.com/apps/pyyyyyycoooon-booooot111',
-        url='https://github.com/apps/octomachinery',
+        name="pycon-2020-github-bot",
+        version="1.0.0",
+        url="https://github.com/apps/octomachinery",
     )
